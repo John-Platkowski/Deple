e33603fcc801f30b8731b23b09468bc60acc693a
andrewwHowell81 <andrewwhowell81@gmail.com>
2026-04-19 00:05:55 -0500

    MVP attempt-3

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Fredoka_One, Nunito } from "next/font/google";
+import { Geist, Geist_Mono, Nunito } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
